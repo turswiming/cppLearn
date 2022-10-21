2f0398c8f6b0455f88e773ab63f5b0ed922e878b
tur <1431839116@qq.com>
2022-10-21 21:58:35 +0800

    临时上传（有编译错误）

--- experiment2_task2/main.cpp
+++ experiment2_task2/main.cpp
@@ -23,7 +23,6 @@ struct Book
     string name;
     string author;
     float price;
-    
 };
 class Library
 {
@@ -34,21 +33,44 @@ public:
         leftcnt = 0;
         rightcnt = 0;
     };
-    bool Add(Book& book){
-        Link<Book>* link = new Link<Book>(book);
-        link->next= fence->next;
-        fence->next=link;
-        fence=fence->next;
-        leftcnt+=1;
+    bool Add(Book &book)
+    {
+        if (FindElem(book))
+        {
+            return false;
+        }
+
+        Link<Book> *link = new Link<Book>(book);
+        link->next = NULL;
+        fence->next = link;
+        fence = fence->next;
+        leftcnt += 1;
         return true;
     };
-    bool Remove(){
-
-    };
-    bool Find(string name,string author){
+    bool Remove(string name, string author)
+    {
         Book book;
-        book.name=name;
-        book.author=author;
+        book.name = name;
+        book.author = author;
+        if (!FindElem(book))
+        {
+            return false;
+        };
+        Link<Book> * nodeToDelate;
+        FindElem(book, &nodeToDelate);
+        Link<Book> *previousNode;
+        GetFrontElem(nodeToDelate, &previousNode);
+        previousNode->next = nodeToDelate->next;
+
+        // delete (&nodeToDelate);
+
+        return true;
+    };
+    bool Find(string name, string author)
+    {
+        Book book;
+        book.name = name;
+        book.author = author;
         return FindElem(book);
     };
 
@@ -58,42 +80,55 @@ private:
     Link<Book> *fence; // Last element on left
     int leftcnt;       // Size of left
     int rightcnt;      // Size of right
-    bool FindElem(Book bookToFind){
-       Link<Book> link =  new Link<Book>();
-        return FindElem(bookToFind,link);
+    bool FindElem(Book bookToFind)
+    {
+        Link<Book> link = new Link<Book>();
+        return FindElem(bookToFind, link);
     }
-    bool FindElem(Book bookToFind,Link<Book> &goal){
-        
+    bool GetFrontElem(Link<Book> &thisElem, Link<Book> &frontElem)
+    {
         Link<Book> *elemReadNow = head;
-        while (elemReadNow->next!=NULL)
+        Link<Book> previousElem;
+        while (elemReadNow->next != NULL)
         {
-            elemReadNow=elemReadNow->next;
-            if ((bookToFind.name==elemReadNow->element.name) && (bookToFind.author==elemReadNow->element.author))
+            previousElem = elemReadNow;
+            elemReadNow = elemReadNow->next;
+            if (&thisElem == elemReadNow)
             {
-                goal=elemReadNow;
+                frontElem = previousElem;
                 return true;
             }
-            
         }
         return false;
-        
+    };
+    bool FindElem(Book bookToFind, Link<Book> *goal)
+    {
+
+        Link<Book> *elemReadNow = head;
+        while (elemReadNow->next != NULL)
+        {
+            elemReadNow = elemReadNow->next;
+            if ((bookToFind.name == elemReadNow->element.name) && (bookToFind.author == elemReadNow->element.author))
+            {
+                goal = elemReadNow;
+                return true;
+            }
+        }
+        return false;
     };
 };
 int main()
 {
-    Library *library =new Library();
-    Book* book=new Book();
-    book->author="haha";
-    book->name="why school of design so fuzzy";
+    Library *library = new Library();
+    Book *book = new Book();
+    book->author = "haha";
+    book->name = "why school of design so fuzzy";
     library->Add(*book);
-    bool b = library->Find("why school of design so fuzzy","haha");
-        cout<<b<<endl;
+    bool b = library->Find("why school of design so fuzzy", "haha");
+    cout << "删除前"<<b << endl;
+    library->Remove("why school of design so fuzzy", "haha");
+    b = library->Find("why school of design so fuzzy", "haha");
+    cout << "删除后"<<b << endl;
 
-         b = library->Find("why school of design so fuzzy","fuck");
-    cout<<b<<endl;
-    Book* book2=new Book();
-    book2->author="fafa";
-    book2->name="why school of design so crazy";
-    library->Add(*book2);
     return 0;
 }
